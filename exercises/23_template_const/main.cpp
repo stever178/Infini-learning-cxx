095a5cb2236b6a546160983ee03518c662c73517
stever178 on win10 <2874146120@qq.com>
2024-12-15 22:37:57 +0800

    [hw] ex0~ex23

--- exercises/23_template_const/main.cpp
+++ exercises/23_template_const/main.cpp
@@ -11,6 +11,10 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
+        for (int i = 0; i < 4; i++) {
+            shape[i] = shape_[i];
+            size *= shape[i];
+        }
         data = new T[size];
         std::memset(data, 0, size * sizeof(T));
     }
@@ -32,10 +36,24 @@ struct Tensor {
 private:
     unsigned int data_index(unsigned int const indices[N]) const {
         unsigned int index = 0;
+
+        T *temp = new T[N];
+        temp[N - 1] = shape[N - 1];
+        for (int i = N - 2; i >= 0; i --) {
+            temp[i] = shape[i] * temp[i + 1];
+        }
+
         for (unsigned int i = 0; i < N; ++i) {
             ASSERT(indices[i] < shape[i], "Invalid index");
             // TODO: 计算 index
+            if (i < N - 1) {
+                index += indices[i] * temp[i];
+            } else {
+                index += indices[i];
+            }
         }
+
+        delete[] temp;
         return index;
     }
 };
